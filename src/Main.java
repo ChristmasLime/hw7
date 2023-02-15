7f6e5e49c6838524c92491dcd4b73704d174d7cf
ChristmasLime <christmasboom@mail.ru>
2023-02-15 19:09:44 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,90 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
-
+        int freeMoney = 15000;
+        int total = 0;
+        int i = 0;
+        while (total <= 2459000) {
+            i++;
+            total = total + freeMoney;
+            System.out.println("месяц № " + i + " сумма накоплений равна " + total + " рублей");
+        }
     }
-
     public static void task2() {
         System.out.println("Задача 2");
 
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i+++" ");
+        }System.out.println();
+        for (i = 10; i > 0; i--) {
+            System.out.print(i+" ");
+        }
     }
-
     public static void task3() {
-        System.out.println("Задача 3");
-
+        System.out.println("\nЗадача 3");
+        int allPeople = 12000000;
+        int birthRate =17;
+        int mortality =8;
+        int difference = birthRate-mortality;
+        int peopleYear = allPeople/1000 *difference;
+        for (int i = 1;i<=10;i++){
+            allPeople = allPeople+peopleYear;
+            System.out.println("Год " +i+", численность населения составляет " +allPeople);
+        }
     }
-
     public static void task4() {
         System.out.println("Задача 4");
-
+        int firstInstallment = 15000;
+        int deposit = (int) (firstInstallment * 0.07);
+        int i = 0;
+        for (int finish =15000; finish <=12000000;finish=finish+deposit ) {
+            i++;
+            System.out.println("Месяц " + i + " накопилось " + finish);
+        }
     }
-
     public static void task5() {
         System.out.println("Задача 5");
-
+        int firstInstallment = 15000;
+        int deposit = (int) (firstInstallment * 0.07);
+        int i = 0;
+        for (int finish = 15000; finish <= 12000000; finish = finish + deposit) {
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " накопилось " + finish);
+            }
+        }
     }
-
     public static void task6() {
         System.out.println("Задача 6");
-
+        int year = 9;
+        int month = year*12;
+        int monthMoney = 15000;
+        int deposit = (int) (monthMoney * 0.07);
+        int finish = 0;
+        for (int i = 1;i<=month;i++) {
+            finish = finish +monthMoney;
+            finish = finish + deposit;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " накопилось " + finish);
+            }
+        }
     }
-
     public static void task7() {
         System.out.println("Задача 7");
-
+        for (int monthDay = 5;monthDay <=31;monthDay = monthDay+7){
+            System.out.println("Сегодня пятница, "+monthDay +" -е число. Необходимо подготовить отчет");
+        }
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        for (int i = 1896; i < 2100; i = i + 79) {
+            System.out.println(i);
+        }
     }
 }
